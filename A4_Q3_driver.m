40062fbe03b1b7d962c230c0a1d485c066deb2c0
friketrike <friketrike@gmail.com>
2016-12-05 12:11:08 -0500

    plotting centroids

--- A4_Q3_driver.m
+++ A4_Q3_driver.m
@@ -1,10 +1,23 @@
+% COMP 6321 Machine Learning, Fall 2016
+% Federico O'Reilly Regueiro - 40012304
+% Assignment 4, due December 9th
 
+
+% Script file for performing k-means clustering. Should return the following:
 %• How many clusters there are in the end. (A cluster can “disappear” in one iteration of the
 %algorithm if no vectors or closest to its centroid.)
 %• The final centroids of each cluster.
 %• The number of pixels associated to each cluster.
 %• The sum of squared distances from each pixel to the nearest centroid after every iteration of the
 %algorithm.
+
+isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
+
+if isOctave
+  flush = @()fflush(stdout);
+else
+  flush = @()drawnow('update');
+end
 
 t0 = tic();
 
@@ -24,34 +37,38 @@ m = length(data(:,1));
 d = length(data(1,:));
 k = length(init_matx(:,1));
 
-% guesstimated number of iterations for preallocation
+% guesstimated number of iterations for preallocation, 
+% This is mostly for efficiency as the loop will stop before if converged
 ITERS = 50;
 
+% Initial centroids or cluster means
 k_means = repmat(reshape(init_matx', 1, d, k), m, 1, 1);
 new_means = init_matx;
 k_labels = 1:m;
 k_settled = false;
+% how many pixels per cluster per iteration
 k_membership_counts = zeros(ITERS, k);
+% keep a trace of the centroids per iteration
 means_trajectory = zeros(k,d,ITERS);
 sum_squared_dist = zeros(1, ITERS);
 iteration_count = 1;
 % keep k-copies of the data to quickly get the distance to centroids
 unfolded_data = repmat(data, 1, 1, k);
 % get the L2 norm of the row-slice of the difference between 8 pixel copies and centroids
-slice_norm = @(tensor)reshape(sum((tensor.^2),2).^0.5, size(tensor, 1), size(tensor, 3));
+slice_sq_norm = @(tensor)reshape(sum((tensor.^2),2), size(tensor, 1), size(tensor, 3));
 
-% Repeat the process until done...
+% Repeat the process until done... 
 while (~k_settled)
   loop = tic();
   disp(sprintf('Clustering all pixels, iteration %d', iteration_count));
-  fflush(stdout);
+  flush();
   k_means = repmat(reshape(new_means', 1, d, k), m, 1, 1);
   k_means_flat = reshape(k_means(1,:,:), d, k)';
-  [min_l2, idxs] = min(slice_norm(unfolded_data - k_means), [], 2);
-  sum_squared_dist(iteration_count) = sum(min_l2.^2);
+  [min_l2_sq, idxs] = min(slice_sq_norm(unfolded_data - k_means), [], 2);
+  sum_squared_dist(iteration_count) = sum(min_l2_sq);
   k_labels = idxs;
   disp(sprintf('Reassigning means'));
-  fflush(stdout);
+  flush();
   for kth = 1:k
       temp = zeros(size(data));
       kth_pixels_idx = (k_labels == kth);
@@ -66,9 +83,9 @@ while (~k_settled)
   disp(sprintf(...
     'Norm of the difference between this iteration and last''s means: %d',...
      norm(new_means-k_means_flat)))
-  fflush(stdout);  
+  flush();
   disp(sprintf('iteration %d took %d seconds.', iteration_count, toc(loop)));
-  fflush(stdout);
+  flush();
   
   % See if we're done
   if (sum(sum(new_means ~= k_means_flat)) == 0)
@@ -108,9 +125,25 @@ disp(k_membership_counts(iteration_count,:));
 
 disp('The final centroids are:');
 disp(k_means_flat)
+g = figure(1)
+for iter = iteration_count:-1:1
+  for kth = 1:k
+    color = means_trajectory(kth, :, iter)./255;
+    h = barh(kth, iter, 'stacked', 'faceColor', color);
+    hold on;
+  end  
+end
+hold off;
+axis([0,iteration_count,0,k+1]);
+title('Colors resulting from centroid movement over iterations')
+xlabel('Iterations');
+ylabel('Cluster');
+saveas(gcf,'centroidEvolutions.pdf');
 
+figure(2)
 plot(sum_squared_dist)
 title('sum of squared distances to centroid per iteration');
 xlabel('iterations');
+saveas(gcf,'sumSquaredDistances.pdf');
 
 disp(sprintf('The whole process took %d seconds.', toc(t0)));
